832ed99e473307611f7ad1c654f81c1ace0190ee
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-03-16 14:26:40 +0100

    [add] events table

--- init.sql
+++ init.sql
@@ -20,4 +20,19 @@ CREATE TABLE IF NOT EXISTS users (
 	PRIMARY KEY (user_id)
 );
 
+CREATE TABLE IF NOT EXISTS events (
+	event_id int NOT NULL UNIQUE AUTO_INCREMENT,
+	event_start varchar(10) NOT NULL,
+	event_end varchar(10) NOT NULL,
+	event_description varchar(150) NOT NULL,
+	event_date date NOT NULL,
+	event_location varchar(50) NOT NULL,
+	event_url varchar(50) NOT NULL,
+	event_name varchar(50) NOT NULL,
+	calendar_id int NOT NULL,
+	PRIMARY KEY (event_id)
+);
+
 ALTER TABLE calendars ADD CONSTRAINT calendars_fk3 FOREIGN KEY (calendar_user_id) REFERENCES users(user_id);
+
+ALTER TABLE events ADD CONSTRAINT events_fk7 FOREIGN KEY (calendar_id) REFERENCES calendars(calendar_id);
