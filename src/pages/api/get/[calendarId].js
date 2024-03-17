a7abd8d9e291a52c7360b261f8ab7eae5113fffb
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-03-17 12:42:22 +0100

    [add] get Api

--- src/pages/api/get/[calendarId].js
+++ src/pages/api/get/[calendarId].js
@@ -16,8 +16,8 @@ async function connectMySQL() {
 export default async function Get(req, res) {
 	const connection = await connectMySQL();
 
-	console.log(req.query.calendarId);
-	console.log(req.headers.user);
+	console.log(req.query.calendarId); // e7488217-b941-491f-bcc1-17077dbc8031,e7488217-b941-491f-bcc1-17077dbc8037
+	console.log(req.query.date); //03-17-2024
 
 	if (!req.headers.user) {
 		return res.status(401).send('unauthorized');
@@ -34,10 +34,37 @@ export default async function Get(req, res) {
 		var decoded = jwt.verify(req.headers.token, user.user_key_public);
 
 		if (decoded.key == user.user_key_private && decoded.id == user.user_id_public && decoded.id == req.headers.user) {
-			if (decoded.calendarId.includes(req.query.calendarId)) {
-				var calendar = await connection.execute('SELECT * FROM calendars WHERE calendar_id_public = ?', [req.query.calendarId]);
-				return res.status(200).send(calendar[0]);
+			var calendarIds = decoded.calendarId;
+			var reqCalendarIds = req.query.calendarId.split(':');
+
+			var calendars = [];
+			var events = [];
+
+			for (let i = 0; i < reqCalendarIds.length; i++) {
+				var calendarId = reqCalendarIds[i];
+				if (!calendarIds.includes(calendarId)) {
+					continue;
+				}
+				var calendar = await connection.execute('SELECT calendar_id, calendar_name, calendar_color, calendar_id_public FROM calendars WHERE calendar_id_public = ?', [calendarId]);
+
+				var dateParts = req.query.date.split('-');
+				var eventDate = new Date(dateParts[2], parseInt(dateParts[0]) - 1, dateParts[1]);
+				eventDate.setDate(eventDate.getDate() - 1);
+
+				var eventsCalendar = await connection.execute('SELECT event_start, event_end, event_description, event_date, event_location, event_url, event_name FROM events WHERE calendar_id = ? AND event_date > ?', [calendar[0][0].calendar_id, eventDate]);
+
+				eventsCalendar[0].map((item) => {
+					item.calendar_id = calendar[0][0].calendar_id_public;
+					item.color = calendar[0][0].calendar_color;
+					events.push(item);
+				});
+
+				delete calendar[0][0].calendar_id;
+
+				calendars.push(calendar[0][0]);
 			}
+
+			return res.status(200).send({ calendars, events });
 		}
 
 		return res.status(401).send('unauthorized');
