7cc24b4fcf45aac5a8bbffff4145cee38faa9b27
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-03-18 11:13:13 +0100

    [add] move reformatEvents function

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,69 @@ import styles from '../styles/index.module.css';
 import { useEffect, useState } from 'react';
 import Loading from '@/components/loading';
 import HeaderComponents from '@/components/header/header';
+function formatTime(timeString) {
+	const [hours, minutes] = timeString.split(':');
+	var formattedHours = hours;
+	var formattedMinutes = minutes;
+	if (hours != '00' && hours.length != 2) {
+		formattedHours = hours < 10 ? `0${hours}` : hours;
+	}
+	if (minutes != '00' && minutes.length != 2) {
+		formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+	}
+	return `${formattedHours}:${formattedMinutes}`;
+}
+
+function reformatEvents(events) {
+	const eventsByDate = {};
+
+	events.forEach((event) => {
+		const date = event.event_date.split('T')[0];
+		if (!eventsByDate[date]) {
+			eventsByDate[date] = [];
+		}
+		eventsByDate[date].push({
+			event_start: event.event_start,
+			event_end: event.event_end,
+			event_description: event.event_description,
+			event_location: event.event_location,
+			event_url: event.event_url,
+			event_name: event.event_name,
+			calendar_id: event.calendar_id,
+			calendar_color: event.color,
+		});
+	});
+
+	const sortedDates = Object.keys(eventsByDate).sort((a, b) => {
+		const dateA = new Date(a);
+		const dateB = new Date(b);
+		return dateA - dateB;
+	});
+
+	const sortedEventsByDate = {};
+	sortedDates.forEach((date) => {
+		sortedEventsByDate[date] = eventsByDate[date];
+
+		sortedEventsByDate[date].sort((a, b) => {
+			const formattedTimeA = formatTime(a.event_start);
+			const formattedTimeB = formatTime(b.event_start);
+
+			const timeA = new Date(`1970-01-01T${formattedTimeA}`);
+			const timeB = new Date(`1970-01-01T${formattedTimeB}`);
+
+			return timeA - timeB;
+		});
+
+		sortedEventsByDate[date].forEach((item) => {
+			console.log('formatTime(item.event_start);', formatTime(item.event_start));
+			console.log('formatTime(item.event_end);', formatTime(item.event_end));
+			item.event_start = formatTime(item.event_start);
+			item.event_end = formatTime(item.event_end);
+		});
+	});
+
+	return sortedEventsByDate;
+}
 
 export default function Home() {
 	const [loading, setIsLoading] = useState(false);
