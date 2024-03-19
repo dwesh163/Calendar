3cbd1b5ac92edfa160e2df5166ddd2224b664342
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-03-19 18:10:46 +0100

    [adð] logout button

--- src/components/side/side.js
+++ src/components/side/side.js
@@ -1,86 +1,16 @@
 import styles from '@/styles/side.module.css';
-import { PersonFill } from 'react-bootstrap-icons';
+import { PersonFill, BoxArrowInLeft } from 'react-bootstrap-icons';
 
 import { useSession, signIn, signOut } from 'next-auth/react';
 import { useState, useEffect } from 'react';
 import Events from './events';
 
-function formatTime(timeString) {
-	const [hours, minutes] = timeString.split(':');
-	var formattedHours = hours;
-	var formattedMinutes = minutes;
-	if (hours != '00' && hours.length != 2) {
-		formattedHours = hours < 10 ? `0${hours}` : hours;
-	}
-	if (minutes != '00' && minutes.length != 2) {
-		formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-	}
-	return `${formattedHours}:${formattedMinutes}`;
-}
-
-function reformatEvents(events) {
-	const eventsByDate = {};
-
-	events.forEach((event) => {
-		const date = event.event_date.split('T')[0];
-		if (!eventsByDate[date]) {
-			eventsByDate[date] = [];
-		}
-		eventsByDate[date].push({
-			event_start: event.event_start,
-			event_end: event.event_end,
-			event_description: event.event_description,
-			event_location: event.event_location,
-			event_url: event.event_url,
-			event_name: event.event_name,
-			calendar_id: event.calendar_id,
-			calendar_color: event.color,
-		});
-	});
-
-	const sortedDates = Object.keys(eventsByDate).sort((a, b) => {
-		const dateA = new Date(a);
-		const dateB = new Date(b);
-		return dateA - dateB;
-	});
-
-	const sortedEventsByDate = {};
-	sortedDates.forEach((date) => {
-		sortedEventsByDate[date] = eventsByDate[date];
-
-		sortedEventsByDate[date].sort((a, b) => {
-			const formattedTimeA = formatTime(a.event_start);
-			const formattedTimeB = formatTime(b.event_start);
-
-			const timeA = new Date(`1970-01-01T${formattedTimeA}`);
-			const timeB = new Date(`1970-01-01T${formattedTimeB}`);
-
-			return timeA - timeB;
-		});
-
-		sortedEventsByDate[date].forEach((item) => {
-			console.log('formatTime(item.event_start);', formatTime(item.event_start));
-			console.log('formatTime(item.event_end);', formatTime(item.event_end));
-			item.event_start = formatTime(item.event_start);
-			item.event_end = formatTime(item.event_end);
-		});
-	});
-
-	console.log('sortedEventsByDate', sortedEventsByDate);
-
-	return sortedEventsByDate;
-}
-
-export default function Side({ calendars, initialDate, events }) {
+export default function Side({ calendars, initialDate, events, eventsLite }) {
 	const { data: session, status } = useSession();
 
-	const formatEvents = reformatEvents(events);
+	const [isHovered, setIsHovered] = useState(false);
 
-	if (formatEvents) {
-		console.log(formatEvents);
-	}
-
-	const daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+	const daysOfWeek = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'];
 	const [currentDate, setCurrentDate] = useState(new Date(initialDate));
 
 	useEffect(() => {
@@ -90,7 +20,7 @@ export default function Side({ calendars, initialDate, events }) {
 	const getDaysInMonth = (date) => {
 		const year = date.getFullYear();
 		const month = date.getMonth();
-		const firstDay = new Date(year, month, 1);
+		const firstDay = new Date(year, month, 0);
 		const lastDay = new Date(year, month + 1, 0).getDate();
 
 		const firstDayOfWeek = firstDay.getDay();
@@ -108,11 +38,13 @@ export default function Side({ calendars, initialDate, events }) {
 		}
 
 		const lastDayOfWeek = new Date(year, month, lastDay).getDay();
-		const remainingDays = 6 - lastDayOfWeek;
-		for (let i = 1; i <= remainingDays; i++) {
-			const day = new Date(year, month + 1, i);
-			day.notInMonth = true;
-			daysArray.push(day);
+		if (lastDayOfWeek != 0) {
+			const remainingDays = 7 - lastDayOfWeek;
+			for (let i = 1; i <= remainingDays; i++) {
+				const day = new Date(year, month + 1, i);
+				day.notInMonth = true;
+				daysArray.push(day);
+			}
 		}
 
 		return daysArray;
@@ -137,11 +69,20 @@ export default function Side({ calendars, initialDate, events }) {
 	return (
 		<div className="left-menu-smAq6k">
 			<div className="top-hDJypf">
-				<div className={styles['user-box']}>
-					<PersonFill className={styles['user-icon']} color="#fff" />
-					<span className={styles['user-name']}>{session ? session.user.name : ''}</span>
-					<button onClick={signOut}>Sign Out</button>
+				<div className={styles['user-box']} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)} onClick={signOut}>
+					{isHovered ? (
+						<span style={{ cursor: isHovered ? 'pointer' : '', display: 'flex', alignItems: 'center' }}>
+							<BoxArrowInLeft className={styles['user-icon']} color="#fff" />
+							<span className={styles['user-name']}>Sign Out</span>
+						</span>
+					) : (
+						<>
+							<PersonFill className={styles['user-icon']} color="#fff" />
+							<span className={styles['user-name']}>{session ? session.user.name : ''}</span>
+						</>
+					)}
 				</div>
+
 				<div className="icon-button">
 					<div className="icon-button-master-fEMv3C icon-button-master" data-id="I1:299;45:4078">
 						<img className="icon" src="https://cdn.animaapp.com/projects/65f14e7781d354160ac606b6/releases/65f14e945ff555309d45bd7e/img/icon.svg" anima-src="https://cdn.animaapp.com/projects/65f14e7781d354160ac606b6/releases/65f14e945ff555309d45bd7e/img/icon.svg" alt="Icon" />
@@ -171,11 +112,10 @@ export default function Side({ calendars, initialDate, events }) {
 				{weeks.map((week, weekIndex) => (
 					<div className={styles['week']} key={weekIndex}>
 						{week.map((day, dayIndex) => {
-							const eventsForDay = events[`${new Date(day).toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }).replaceAll('/', '-').split('-')[2]}-${new Date(day).toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }).replaceAll('/', '-').split('-')[0]}-${new Date(day).toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }).replaceAll('/', '-').split('-')[1]}`] || [];
-
+							const eventsForDay = eventsLite[`${new Date(day).toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }).replaceAll('/', '-').split('-')[2]}-${new Date(day).toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }).replaceAll('/', '-').split('-')[0]}-${new Date(day).toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' }).replaceAll('/', '-').split('-')[1]}`] || [];
 							return (
 								<div className={styles['day-box']} key={dayIndex}>
-									{new Date().getDate() === day.getDate() && new Date().getMonth() === day.getMonth() ? (
+									{new Date().getDate() === day.getDate() && new Date().getMonth() === day.getMonth() && new Date().getFullYear() === day.getFullYear() ? (
 										<div class={styles['day-current']}>
 											<div className={`${!isSameMonth(day, currentDate) ? styles['day-not'] : ''} valign-text-middle inter-semi-bold-white-11px`} style={{ marginTop: '5px' }}>
 												{day.getDate()}
@@ -203,7 +143,7 @@ export default function Side({ calendars, initialDate, events }) {
 				))}
 			</div>
 
-			<Events events={formatEvents} />
+			<Events events={events} />
 		</div>
 	);
 }
