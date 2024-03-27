5d82ca69f8c53dc250130e172ad74b9d613c62de
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-03-27 14:19:23 +0100

    [add] var

--- src/pages/api/get/lists/[calendarId].js
+++ src/pages/api/get/lists/[calendarId].js
@@ -2,7 +2,7 @@ import mysql from 'mysql2/promise';
 import { dbConfig } from '../../../../../lib/config';
 import { getSession } from 'next-auth/react';
 
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 async function connectMySQL() {
 	try {
@@ -19,35 +19,35 @@ export default async function Get(req, res) {
 	const session = await getSession({ req });
 
 	try {
-		var user = await connection.execute('SELECT * FROM users WHERE user_id_public = ?', [session.user.id]);
+		let user = await connection.execute('SELECT * FROM users WHERE user_id_public = ?', [session.user.id]);
 		user = user[0][0];
 
 		if (user == undefined) {
 			return res.status(401).send('unauthorized');
 		}
 
-		var calendarIds = await connection.execute('SELECT calendar_id_public FROM calendars WHERE calendar_user_id = ?', [user.user_id]);
+		let calendarIds = await connection.execute('SELECT calendar_id_public FROM calendars WHERE calendar_user_id = ?', [user.user_id]);
 		calendarIds = calendarIds[0].map((item) => item.calendar_id_public);
 
-		var reqCalendarIds = req.query.calendarId.split(':');
+		let reqCalendarIds = req.query.calendarId.split(':');
 
-		var calendars = [];
-		var events = [];
+		let calendars = [];
+		let events = [];
 
 		for (let i = 0; i < reqCalendarIds.length; i++) {
-			var calendarId = reqCalendarIds[i];
+			let calendarId = reqCalendarIds[i];
 			if (!calendarIds.includes(calendarId)) {
 				continue;
 			}
-			var calendar = await connection.execute('SELECT calendar_id, calendar_name, calendar_color, calendar_id_public FROM calendars WHERE calendar_id_public = ?', [calendarId]);
+			let calendar = await connection.execute('SELECT calendar_id, calendar_name, calendar_color, calendar_id_public FROM calendars WHERE calendar_id_public = ?', [calendarId]);
 
-			var eventDateWeek = new Date();
-			var dateParts = req.query.date.split('-');
-			var eventDate = new Date(dateParts[2], parseInt(dateParts[0]) - 1, dateParts[1]);
+			let eventDateWeek = new Date();
+			let dateParts = req.query.date.split('-');
+			let eventDate = new Date(dateParts[2], parseInt(dateParts[0]) - 1, dateParts[1]);
 			eventDate.setDate(eventDate.getDate() - 1);
 			eventDateWeek.setDate(eventDate.getDate() + 7);
 
-			var eventsCalendar = await connection.execute('SELECT event_start, event_end, event_description, event_date, event_location, event_url, event_name FROM events WHERE calendar_id = ? AND event_date > ? AND event_date < ?', [calendar[0][0].calendar_id, eventDate, eventDateWeek]);
+			let eventsCalendar = await connection.execute('SELECT event_start, event_end, event_description, event_date, event_location, event_url, event_name FROM events WHERE calendar_id = ? AND event_date > ? AND event_date < ?', [calendar[0][0].calendar_id, eventDate, eventDateWeek]);
 
 			eventsCalendar[0].map((item) => {
 				item.calendar_id = calendar[0][0].calendar_id_public;
