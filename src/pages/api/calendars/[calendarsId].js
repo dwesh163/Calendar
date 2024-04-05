13bbf567c88a3b91e7571d7aa6c04f1bdd7015b1
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-05 17:25:32 +0200

    [add] new api

---
+++ src/pages/api/calendars/[calendarsId].js
@@ -0,0 +1,90 @@
+import mysql from 'mysql2/promise';
+import { dbConfig } from '../../../../lib/config';
+import getAndVerifyUser from '../../../../lib/auth';
+
+async function connectMySQL() {
+	try {
+		const connection = await mysql.createConnection(dbConfig);
+		return connection;
+	} catch (error) {
+		console.error('Error connecting to MySQL:', error);
+		throw error;
+	}
+}
+
+export default async function Auth(req, res) {
+	try {
+		const connection = await connectMySQL();
+		if (req.method == 'GET') {
+			const verify = await getAndVerifyUser(req, 'GET', connection);
+			let calendars = [];
+
+			if (verify.code != 200) {
+				return res.status(verify.code).send(verify.response);
+			}
+
+			const reqCalendarsId = req.query.calendarsId.split(/:|\n/).filter((id) => id.trim() !== '');
+
+			for (const calendarId of reqCalendarsId) {
+				if (verify.response.calendarsId.includes(calendarId)) {
+					const [[calendar]] = await connection.execute('SELECT calendar_name, calendar_color, calendar_id_public FROM calendars WHERE calendar_id_public = ?', [calendarId]);
+					calendars.push(calendar);
+				}
+			}
+
+			return res.status(200).send(calendars);
+		}
+
+		if (req.method == 'PUT') {
+			const verify = await getAndVerifyUser(req, 'PUT', connection);
+			const data = JSON.parse(JSON.stringify(req.body));
+			const Regex = /^#[0-9A-F]{6}$/i;
+			const id = req.query.calendarsId;
+
+			if (verify.code != 200) {
+				return res.status(verify.code).send(verify.response);
+			}
+
+			if (!data.name && !data.color && !data.icon) {
+				return res.status(406).send('No new data provided');
+			}
+
+			if (data.color && !Regex.test(data.color)) {
+				return res.status(406).send('Color format not valid must be #XXXXXX');
+			}
+
+			const [[existingCalendar]] = await connection.execute('SELECT * FROM calendars WHERE calendar_id_public = ? AND calendar_user_id = ?', [id, verify.response.user_id]);
+			const newName = data.name ? data.name : existingCalendar.calendar_name;
+			const newIcon = data.icon ? data.icon : existingCalendar.calendar_icon;
+			const newColor = data.color ? data.color.replace('#', '') : existingCalendar.calendar_color;
+
+			await connection.execute('UPDATE calendars SET calendar_name = ?, calendar_color = ?, calendar_icon = ? WHERE calendar_id_public = ? AND calendar_user_id = ?', [newName, newColor, newIcon, id, verify.response.user_id]);
+
+			return res.status(200).send('Calendar updated successfully');
+		}
+
+		if (req.method == 'DELETE') {
+			const verify = await getAndVerifyUser(req, 'DELETE', connection);
+			if (verify.code != 200) {
+				return res.status(verify.code).send(verify.response);
+			}
+
+			const reqCalendarsId = req.query.calendarsId.split(/:|\n/).filter((id) => id.trim() !== '');
+
+			for (const calendarId of reqCalendarsId) {
+				if (verify.response.calendarsId.includes(calendarId)) {
+					const [[calendar]] = await connection.execute('SELECT calendar_id FROM calendars WHERE calendar_id_public = ?', [calendarId]);
+
+					await connection.execute('DELETE FROM events WHERE calendar_id = ?', [calendar.calendar_id]);
+					await connection.execute('DELETE FROM calendars WHERE calendar_id_public = ?', [calendarId]);
+				}
+			}
+
+			res.send(200);
+		}
+		return res.status(405).send('Method Not Allowed');
+	} catch (error) {
+		console.log(error);
+		return res.status(401).send('unauthorized');
+	}
+}
---
+++ src/pages/api/calendars/index.js
@@ -0,0 +1,70 @@
+import mysql from 'mysql2/promise';
+import { dbConfig } from '../../../../lib/config';
+import getAndVerifyUser from '../../../../lib/auth';
+import { v4 as uuidv4 } from 'uuid';
+
+async function connectMySQL() {
+	try {
+		const connection = await mysql.createConnection(dbConfig);
+		return connection;
+	} catch (error) {
+		console.error('Error connecting to MySQL:', error);
+		throw error;
+	}
+}
+
+export default async function Index(req, res) {
+	try {
+		const connection = await connectMySQL();
+		if (req.method == 'GET') {
+			const verify = await getAndVerifyUser(req, 'GET', connection);
+
+			if (verify.code != 200) {
+				return res.status(verify.code).send(verify.response);
+			}
+
+			let [calendarsId] = await connection.execute('SELECT calendar_id_public, calendar_name, calendar_icon, calendar_color FROM calendars WHERE calendar_user_id = ?', [verify.response.user_id]);
+
+			calendarsId = calendarsId.map((item) => ({
+				id: item.calendar_id_public,
+				color: item.calendar_color,
+				name: item.calendar_name,
+				icon: item.calendar_icon,
+			}));
+
+			return res.status(200).send(calendarsId);
+		}
+
+		if (req.method == 'POST') {
+			const verify = await getAndVerifyUser(req, 'POST', connection);
+			const data = JSON.parse(JSON.stringify(req.body));
+			const Regex = /^#[0-9A-F]{6}$/i;
+			const id = uuidv4();
+
+			if (verify.code != 200) {
+				return res.status(verify.code).send(verify.response);
+			}
+
+			if (!data.name || !data.color || !data.icon) {
+				return res.status(406).send('Data not valid');
+			}
+
+			if (!Regex.test(data.color)) {
+				return res.status(406).send('Color format not valid must be #XXXXXX');
+			}
+
+			const [[count]] = await connection.execute('SELECT COUNT(*) AS calendar_count FROM calendars WHERE calendar_user_id = ?', [verify.response.user_id]);
+
+			if (count.calendar_count >= 8) {
+				return res.status(429).send('Maximum number of calendars created');
+			}
+
+			await connection.execute('INSERT INTO calendars (calendar_id_public, calendar_name, calendar_color, calendar_icon, calendar_user_id) VALUES (?, ?, ?, ?,?)', [id, data.name, data.color.replace('#', ''), data.icon, verify.response.user_id]);
+			return res.status(201).send({ id: id, name: data.name, color: data.color.replace('#', ''), icon: data.icon });
+		}
+		return res.status(405).send('Method Not Allowed');
+	} catch (error) {
+		console.log(error);
+		return res.status(401).send('unauthorized');
+	}
+}
