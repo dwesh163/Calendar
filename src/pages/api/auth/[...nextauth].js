2c980da3e0c6ded87550c011544348de6ce0f54b
Dwesh163 <ehouarn.duriaux@protonmail.com>
2024-04-07 13:40:15 +0200

    [fix] User id in devices

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -28,15 +28,14 @@ export const authOptions = (req) => ({
 		async signIn({ user, account, profile, email, credentials }) {
 			const connection = await connectMySQL();
 			try {
-				const [existingUser] = await connection.execute('SELECT * FROM users WHERE user_email = ?', [user.email]);
+				const [[existingUser]] = await connection.execute('SELECT * FROM users WHERE user_email = ?', [user.email]);
 
-				if (existingUser.length) {
+				if (existingUser) {
 					await connection.execute('UPDATE users SET user_username = ?, user_image = ?, user_provider = ?, user_company = ?, user_name = ? WHERE user_email = ?', [profile.login, user.image, account.provider, profile.company, profile.name, user.email]);
 				} else {
 					await connection.execute('INSERT INTO users (user_id_public, user_email, user_username, user_image, user_provider, user_company, user_name) VALUES (?, ?, ?, ?, ?, ?, ?)', [uuidv4(), user.email, profile.login, user.image, account.provider, profile.company, profile.name]);
-					const [rows] = await connection.execute('SELECT user_id FROM users WHERE user_email = ?', [user.email]);
-					const userId = rows[0].user_id;
-					await connection.execute('INSERT INTO calendars (calendar_name, calendar_icon, calendar_id_public, calendar_color, calendar_user_id) VALUES (?, ?, ?, ?, ?)', ['Calendar 1', 'Calendar3', uuidv4(), 'a855f7', userId]);
+					const [[user]] = await connection.execute('SELECT user_id FROM users WHERE user_email = ?', [user.email]);
+					await connection.execute('INSERT INTO calendars (calendar_name, calendar_icon, calendar_id_public, calendar_color, calendar_user_id) VALUES (?, ?, ?, ?, ?)', ['Calendar 1', 'Calendar3', uuidv4(), 'a855f7', user.user_id]);
 				}
 
 				var deviceInfo = uaParser(req.headers['user-agent']);
@@ -46,7 +45,7 @@ export const authOptions = (req) => ({
 				deviceInfo.device.model = deviceInfo.device.model || '';
 				deviceInfo.device.type = deviceInfo.device.type || '';
 
-				await connection.execute('INSERT INTO devices (devices_ip, devices_os_name, devices_os_version, devices_vendor, devices_browser_name, devices_browser_version, devices_public_id, devices_user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [req.headers.host, deviceInfo.os.name, deviceInfo.os.version, deviceInfo.device.vendor, deviceInfo.browser.name, deviceInfo.browser.version, uuidv4(), existingUser[0].user_id]);
+				await connection.execute('INSERT INTO devices (devices_ip, devices_os_name, devices_os_version, devices_vendor, devices_browser_name, devices_browser_version, devices_public_id, devices_user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [req.headers.host, deviceInfo.os.name, deviceInfo.os.version, deviceInfo.device.vendor, deviceInfo.browser.name, deviceInfo.browser.version, uuidv4(), existingUser.user_id ? existingUser.user_id : user.user_id]);
 
 				if (profile.login == process.env.SUPER_ADMIN) {
 					return Promise.resolve(true);
@@ -65,11 +64,11 @@ export const authOptions = (req) => ({
 			const connection = await connectMySQL();
 
 			try {
-				const [existingUser] = await connection.execute('SELECT * FROM users WHERE user_email = ?', [session.user.email]);
-				const [calendarsId] = await connection.execute('SELECT calendar_id_public FROM calendars WHERE calendar_user_id = ?', [existingUser[0].user_id]);
-				if (existingUser.length) {
-					session.user.id = existingUser[0].user_id_public;
-					session.user.username = existingUser[0].user_username;
+				const [[existingUser]] = await connection.execute('SELECT * FROM users WHERE user_email = ?', [session.user.email]);
+				const [calendarsId] = await connection.execute('SELECT calendar_id_public FROM calendars WHERE calendar_user_id = ?', [existingUser.user_id]);
+				if (existingUser) {
+					session.user.id = existingUser.user_id_public;
+					session.user.username = existingUser.user_username;
 					session.user.calendarId = calendarsId.map((item) => item.calendar_id_public);
 				}
 			} catch (error) {
